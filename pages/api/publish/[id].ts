0bcf74c9fcd5162d0307bb2580834f0fc7f797cf
Omotolani <torhlarny15@gmail.com>
2021-11-13 13:04:41 +0100

    I am proud of myself V :wrench:

--- pages/api/publish/[id].ts
+++ pages/api/publish/[id].ts
@@ -1,9 +1,16 @@
 // pages/api/publish/[id].ts
 
 import prisma from '../../../lib/prisma';
+import NextCors from 'nextjs-cors';
 
 // PUT /api/publish/:id
 export default async function handle(req, res) {
+    await NextCors(req, res, {
+        // Options
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
   const postId = req.query.id;
   const post = await prisma.post.update({
     where: { id: Number(postId) },
