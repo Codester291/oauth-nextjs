11c065e3568642fac1ed11e971b25ba149cafce2
Omotolani <torhlarny15@gmail.com>
2021-11-13 12:52:55 +0100

    I am proud of myself IV :wrench:

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -1,7 +1,15 @@
 import prisma from "../../../lib/prisma";
+import NextCors from 'nextjs-cors';
 
 // DELETE /api/post/:id
 export default async function handle(req, res) {
+    
+    await NextCors(req, res, {
+        // Options
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
     const postId = req.query.id;
     if (req.method === 'DELETE') {
         const post = await prisma.post.delete({
